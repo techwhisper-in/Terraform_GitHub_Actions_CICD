66b1b6301254fc01943faec772cdd37415454bf6
Ankit Kumar <technicalcodelab@gmail.com>
2024-07-26 11:33:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "neeha-s3bucket-date"
+    bucket         = "ankit-ex-kumar-s3bucket-7-26-2024"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "neeha-date-dblocks"
+    dynamodb_table = "ankit-ex-kumar-7-26-2024-dblocks"
     encrypt        = true
   }
 }
